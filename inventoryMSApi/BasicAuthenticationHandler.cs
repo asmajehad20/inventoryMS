633c7bff38b6ac9bf73bcc2dca8679ac8ee31515
asmajehad20 <126648311+asmajehad20@users.noreply.github.com>
2024-04-06 15:58:03 +0200

    Add files via upload

---
+++ inventoryMSApi/BasicAuthenticationHandler.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using inventoryMSLogic.src.BusinessLogicLayer;
+
+/// <summary>
+/// Handler for basic authentication.
+/// </summary>
+public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public BasicAuthenticationHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock)
+        : base(options, logger, encoder, clock)
+    {
+    }
+
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        if (!Request.Headers.ContainsKey("Authorization"))
+        {
+            return AuthenticateResult.Fail("Missing Authorization Header");
+        }
+
+        try
+        {
+            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers.Authorization);
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':', 2);
+            var username = credentials[0];
+            var password = credentials[1];
+
+
+            if (AuthenticationManager.CheckUserCredentials(username, password))
+            {
+                var claims = new[]
+                {
+                new Claim(ClaimTypes.Name, username),
+                };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                return AuthenticateResult.Success(new AuthenticationTicket(principal, Scheme.Name));
+            }
+            else
+            {
+                return AuthenticateResult.Fail("Invalid username or password");
+            }
+        }
+        catch
+        {
+            return AuthenticateResult.Fail("Invalid Authorization Header");
+        }
+    }
+
+    
+}
